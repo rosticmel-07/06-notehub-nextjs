9e1ca1af9d58eb2f301f28a0673fd4c674dd44d6
Rostik <rosticmel@gmail.com>
2026-05-06 20:11:58 +0300

    fix 01

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -17,6 +17,7 @@ export default function NoteDetailsClient() {
     queryKey: ["note", id],
     queryFn: () => fetchNoteById(id),
     enabled: Boolean(id),
+    refetchOnMount: false,
   });
 
   if (isLoading) {
